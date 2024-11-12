069fdf18d1e7e1308e91aab6ac0146c8da084003
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-11-11 21:03:06 -0700

    Interactive Components

--- app/components/rental/image-test.js
+++ app/components/rental/image-test.js
@@ -8,10 +8,10 @@ module('Integration | Component | rental/image', function (hooks) {
   test('it renders the given image', async function (assert) {
     await render(hbs`
             `);
-            assert
-            .dom('.image img')
-            .exists()
-            .hasAttribute('src', '/assets/images/teaching-tomster.png')
-            .hasAttribute('alt', 'Teaching Tomster');
-        });
-      });
+    assert
+      .dom('.image img')
+      .exists()
+      .hasAttribute('src', '/assets/images/teaching-tomster.png')
+      .hasAttribute('alt', 'Teaching Tomster');
+  });
+});
