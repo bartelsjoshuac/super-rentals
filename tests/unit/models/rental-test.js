9cb893788e2b9e853ecd0186e70d10a2b5edd568
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-11-11 23:32:13 -0700

    Fixing broken tests.

--- tests/unit/models/rental-test.js
+++ tests/unit/models/rental-test.js
@@ -4,7 +4,6 @@ import { module, test } from 'qunit';
 module('Unit | Model | rental', function (hooks) {
   setupTest(hooks);
 
-
   test('it has the right type', function (assert) {
     const store = this.owner.lookup('service:store');
     let rental = store.createRecord('rental', {
