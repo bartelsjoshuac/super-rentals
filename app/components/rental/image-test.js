4f83d691f74c2a12bc6b243c9215e898d8b0e183
bartelsjoshuac <bartelsjoshuac@gmail.com>
2024-11-11 20:54:07 -0700

    More about components

---
+++ app/components/rental/image-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'super-rentals/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | rental/image', function (hooks) {
+  setupRenderingTest(hooks);
+  test('it renders the given image', async function (assert) {
+    await render(hbs`
+            `);
+            assert
+            .dom('.image img')
+            .exists()
+            .hasAttribute('src', '/assets/images/teaching-tomster.png')
+            .hasAttribute('alt', 'Teaching Tomster');
+        });
+      });
